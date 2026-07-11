d0f65b63e7778497bf578f651d2eaab6dcfe8c86
Arynkr07 <144228549+Arynkr07@users.noreply.github.com>
2026-07-11 17:01:48 +0530

    Time: 668 ms (21.09%) | Memory: 43.1 MB (42.36%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -3,18 +3,16 @@ class Solution {
         int count=0;
         for(int i=0;i<s.length(); i++){
             for(int j=i ; j<s.length();j++){
-                boolean pal = true;
                 int left =i;
                 int right = j;
                 while(left <right){
                     if(s.charAt(left) != s.charAt(right)){
-                        pal = false;
                         break;
                     }
                     left++;
                     right--;
                 }
-                if(pal) count++;
+                if(left>=right) count++;
             }
         }
         return count;
